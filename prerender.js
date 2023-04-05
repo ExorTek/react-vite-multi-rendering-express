a2da4aba9cbbfe4de35cbeac799b2428aeb1d4d9
exortek <exortek979@gmail.com>
2023-04-05 13:57:01 +0300

    fix & refactored

--- prerender.js
+++ prerender.js
@@ -5,7 +5,7 @@ import {fileURLToPath} from 'url'
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const absolutePath = (p) => path.resolve(__dirname, p)
 
-const template = fs.readFileSync(absolutePath('dist/static/index.html'), 'utf-8')
+const template = fs.readFileSync(absolutePath('dist/client/index.html'), 'utf-8')
 const {render} = await import('./dist/server/entry-server.js')
 
 const routesToPrerender = fs
@@ -18,9 +18,9 @@ const routesToPrerender = fs
 (async () => {
     for (const url of routesToPrerender) {
         const context = {}
-        const appHtml = await render(url, context)
+        const appHtml = render(url, context)
         const html = template.replace(`<!--ssr-outlet-->`, appHtml)
-        const filePath = `dist/static${url === '/' ? '/index' : url}.html`
+        const filePath = `dist/client${url === '/' ? '/index' : url}.html`
         fs.writeFileSync(absolutePath(filePath), html)
         console.log('pre-rendered:', filePath)
     }
--- server.js
+++ server.js
@@ -3,81 +3,78 @@ import path from 'path';
 import {fileURLToPath} from 'url';
 import express from 'express';
 
-const port = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5001;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const absolutePath = (p) => path.resolve(__dirname, p);
 
-export async function createServer(
-  root = process.cwd(),
-  isDev = process.env.NODE_ENV === 'development',
-  hmrPort,
-) {
-  const dirname = path.dirname(fileURLToPath(import.meta.url))
-  const absolutePath = (p) => path.resolve(dirname, p)
+const isDev = process.env.NODE_ENV === 'development';
 
-  const indexProd = isDev
-    ? ''
-    : fs.readFileSync(absolutePath('dist/client/index.html'), 'utf-8')
+export async function createServer(root = process.cwd(), hmrPort) {
+    const indexProd = isDev
+        ? ''
+        : fs.readFileSync(absolutePath('dist/client/index.html'), 'utf-8')
 
-  const app = express()
-  let vite
-  if (isDev) {
-    vite = await (
-      await import('vite')
-    ).createServer({
-      root,
-      logLevel: 'info',
-      server: {
-        middlewareMode: true,
-        watch: {
-          usePolling: true,
-          interval: 100,
-        },
-        hmr: {
-          port: hmrPort,
-        },
-      },
-      appType: 'custom',
-    })
-    app.use(vite.middlewares)
-  } else {
-    app.use((await import('compression')).default())
-    app.use(
-      (await import('serve-static')).default(absolutePath('dist/client'), {
-        index: false,
-      }),
-    )
-  }
-
-  app.use('*', async (req, res) => {
-    try {
-      const url = req.originalUrl;
-      let template, render;
-      if (isDev) {
-        template = fs.readFileSync(absolutePath('index.html'), 'utf-8')
-        template = await vite.transformIndexHtml(url, template);
-        render = (await vite.ssrLoadModule('/src/entry-server.jsx')).render;
-      } else {
-        template = indexProd;
-        render = (await import('./dist/server/entry-server.js')).render;
-      }
-      const context = {};
-      const appHtml = render(url, context);
-
-      if (context.url) return res.redirect(301, context.url);
-      const html = template.replace(`<!--ssr-outlet-->`, appHtml);
-      res.status(200).set({'Content-Type': 'text/html'}).end(html);
-    } catch (e) {
-      isDev && vite.ssrFixStacktrace(e)
-      console.log(e.stack)
-      res.status(500).end(e.stack)
+    const app = express()
+    let vite;
+    if (isDev) {
+        vite = await (
+            await import('vite')
+        ).createServer({
+            root,
+            logLevel: 'info',
+            server: {
+                middlewareMode: true,
+                watch: {
+                    usePolling: true,
+                    interval: 100,
+                },
+                hmr: {
+                    port: hmrPort,
+                },
+            },
+            appType: 'custom',
+        })
+        app.use(vite.middlewares)
+    } else {
+        app.use((await import('compression')).default())
+        app.use(
+            (await import('serve-static')).default(absolutePath('dist/client'), {
+                index: false,
+            }),
+        )
     }
-  })
-  return {app}
+
+    app.all('*', async (req, res) => {
+        const url = req.originalUrl;
+
+        const template = isDev
+            ? (await vite.transformIndexHtml(url, fs.readFileSync(absolutePath('index.html'), 'utf-8')))
+            : indexProd;
+
+        const render = isDev
+            ? (await vite.ssrLoadModule('/src/entry-server.jsx')).render
+            : (await import('./dist/server/entry-server.js')).render;
+
+        const context = {};
+        const appHtml = render(url, context);
+        if (context.url) return res.redirect(301, context.url);
+        const html = template.replace(`<!--ssr-outlet-->`, appHtml);
+        res.status(200).set({'Content-Type': 'text/html'}).end(html);
+    })
+    app.use((err, req, res, next) => {
+        console.error(err.stack)
+        res.status(500).send({
+            message: err.message,
+            stack: isDev ? err.stack : undefined,
+        })
+    })
+    return {app, port: PORT}
 }
 
-createServer().then(({app}) =>
-  app.listen(port, () => {
-    console.log(
-      `Server running at http://localhost:${port}/`,
-    )
-  }),
+createServer().then(({app, port}) =>
+    app.listen(port, () => {
+        console.log(
+            `Server running at http://localhost:${port}/`,
+        )
+    }),
 )
